77eafb05e9374129c0e0997e6f30749078ae80d6
Jan Fischer <fischja10@gmail.com>
2017-10-23 21:23:12 +0200

    'loginform'

---
+++ help/help2/app1.js
@@ -0,0 +1,10 @@
+ Datentr„ger in Laufwerk C: ist Windows
+ Volumeseriennummer: C0AD-F0FB
+
+ Verzeichnis von C:\Users\Fischerja\Documents\Git\help\help2
+
+23.10.2017  21:17    <DIR>          .
+23.10.2017  21:17    <DIR>          ..
+23.10.2017  21:17                 0 app1.js
+               1 Datei(en),              0 Bytes
+               2 Verzeichnis(se), 130.268.512.256 Bytes frei
---
+++ help/help2/app2.js
@@ -0,0 +1,11 @@
+ Datentr„ger in Laufwerk C: ist Windows
+ Volumeseriennummer: C0AD-F0FB
+
+ Verzeichnis von C:\Users\Fischerja\Documents\Git\help\help2
+
+23.10.2017  21:17    <DIR>          .
+23.10.2017  21:17    <DIR>          ..
+23.10.2017  21:17               375 app1.js
+23.10.2017  21:17                 0 app2.js
+               2 Datei(en),            375 Bytes
+               2 Verzeichnis(se), 130.268.512.256 Bytes frei
