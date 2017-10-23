cda197eda8efb992d3436886dd0fed7022b30ca5
Jan Fischer <fischja10@gmail.com>
2017-10-23 21:10:13 +0200

    Inital Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+ Datentr„ger in Laufwerk C: ist Windows
+ Volumeseriennummer: C0AD-F0FB
+
+ Verzeichnis von C:\Users\Fischerja\Documents\Git
+
+23.10.2017  21:02    <DIR>          .
+23.10.2017  21:02    <DIR>          ..
+23.10.2017  21:02                 0 app.js
+               1 Datei(en),              0 Bytes
+               2 Verzeichnis(se), 130.262.663.168 Bytes frei
